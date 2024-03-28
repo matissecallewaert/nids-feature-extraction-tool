d2c044b108b713d4631fb46186c3f50002f8c1fd
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-03-28 12:07:45 +0100

    :test_tube: Test 30

--- feature-extraction-tool/src/main.rs
+++ feature-extraction-tool/src/main.rs
@@ -46,14 +46,14 @@ async fn handle_realtime(interface: String) -> Result<(), anyhow::Error> {
 
     // Bump the memlock rlimit. This is needed for older kernels that don't use the
     // new memcg based accounting, see https://lwn.net/Articles/837122/
-    // let rlim = libc::rlimit {
-    //     rlim_cur: libc::RLIM_INFINITY,
-    //     rlim_max: libc::RLIM_INFINITY,
-    // };
-    // let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    // if ret != 0 {
-    //     debug!("remove limit on locked memory failed, ret is: {}", ret);
-    // }
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {}", ret);
+    }
 
     // This will include your eBPF object file as raw bytes at compile-time and load it at
     // runtime. This approach is recommended for most real-world use cases. If you would
